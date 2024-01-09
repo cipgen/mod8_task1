cfeb85621ab657a9174a389cdda255da852d86b6
Yuriy S <shkvarenko@gmail.com>
2024-01-08 23:31:29 -0500

    test

--- install_pre_commit.py
+++ install_pre_commit.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import platform
 import subprocess
+import sys
 
 def get_os():
     os_name = platform.system()
@@ -46,21 +47,38 @@ def setup_pre_commit_config():
   - repo: https://github.com/gitleaks/gitleaks
     rev: v8.16.1
     hooks:
-      - id: gitleaks"""
+      - id: gitleaks
+        args: ['--verbose']  # Add the --verbose option for more detailed results
+        name: Gitleaks with verbose output"""
     with open('.pre-commit-config.yaml', 'w') as config_file:
         config_file.write(config_content)
 
-def install_pre_commit_tools(os_name):
-    if install_package(os_name, "pre-commit"):
-        setup_pre_commit_config()
-        subprocess.call(["pre-commit", "install"])
-        subprocess.call(["pre-commit", "autoupdate"])
+def install_pre_commit_tools(os_name, enable_gitleaks):
+    if enable_gitleaks:
+        if install_package(os_name, "pre-commit"):
+            setup_pre_commit_config()
+            subprocess.call(["pre-commit", "install"])
+            subprocess.call(["pre-commit", "autoupdate"])
+    else:
+        print("Gitleaks is disabled.")
 
 def main():
     os_name = get_os()
     print("Operating system: {}".format(os_name))
+    
+    # Read the enable option from git config
+    enable_gitleaks = False
+    try:
+        enable_gitleaks_str = subprocess.check_output(["git", "config", "--local", "--get", "gitleaks.enable"]).strip().decode()
+        if enable_gitleaks_str.lower() == "true":
+            enable_gitleaks = True
+    except subprocess.CalledProcessError:
+        pass  # Option is not set
+    
+    print("Gitleaks enabled: {}".format(enable_gitleaks))
+    
     if os_name != 'unknown':
-        install_pre_commit_tools(os_name)
+        install_pre_commit_tools(os_name, enable_gitleaks)
     else:
         print("Unknown OS, unable to install tools.")
 
