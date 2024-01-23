cf196df900ad945e42fa8ab770a5341431ca8485
Yuriy S <shkvarenko@gmail.com>
2024-01-23 10:02:41 -0500

    fix commet

--- install_pre_commit.py
+++ install_pre_commit.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 import platform
 import subprocess
 import sys
 
 def get_os():
+    # Detect the operating system
     os_name = platform.system()
     if os_name == 'Darwin':
         return 'darwin'
@@ -14,6 +16,7 @@ def get_os():
         return 'unknown'
 
 def is_command_available(command):
+    # Check if a command is available in the system
     try:
         process = subprocess.Popen([command, "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
@@ -22,13 +25,14 @@ def is_command_available(command):
         return False
 
 def install_package(os_name, package_name):
+    # Install a package based on the operating system
     try:
         if os_name == 'darwin':
             subprocess.call(["brew", "install", package_name])
         elif os_name == 'windows':
             subprocess.call(["pip", "install", package_name])
         elif os_name == 'linux':
-            # Используйте pip для установки pre-commit на Linux
+            # Use pip to install pre-commit on Linux
             if package_name == "pre-commit":
                 subprocess.call(["pip", "install", package_name])
             elif is_command_available("apt-get"):
@@ -45,6 +49,7 @@ def install_package(os_name, package_name):
         return False
 
 def setup_pre_commit_config():
+    # Set up the configuration for pre-commit
     config_content = """repos:
   - repo: https://github.com/gitleaks/gitleaks
     rev: v8.16.1
@@ -56,6 +61,7 @@ def setup_pre_commit_config():
         config_file.write(config_content)
 
 def install_pre_commit_tools(os_name, enable_gitleaks):
+    # Install pre-commit tools and optionally enable gitleaks
     if enable_gitleaks:
         if install_package(os_name, "pre-commit"):
             setup_pre_commit_config()
@@ -65,6 +71,7 @@ def install_pre_commit_tools(os_name, enable_gitleaks):
         print("Gitleaks is disabled.")
 
 def main():
+    # Main function
     os_name = get_os()
     print("Operating system: {}".format(os_name))
     
